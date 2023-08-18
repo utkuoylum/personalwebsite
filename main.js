018fec4819fc2019a30f6ed52f20575da99d682c
Etem Utku Oylum <utkuoylum@gmail.com>
2023-08-18 14:59:47 +0200

    scroll animations

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
+
+menuIcon.addEventListener("click", e => {
+  menuIcon.classList.toggle("bx-x")
+  navbar.classList.toggle("active");
+})
 // scroll sections
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
@@ -18,7 +23,7 @@ let navLinks = document.querySelectorAll("header nav a");
 window.onscroll = (e) => {
 sections.forEach(sec => {
   let top = window.scrollY;
-  let offset = sec.offsetTop - 100;
+  let offset = sec.offsetTop - 110;
   let height = sec.offsetHeight;
   let id = sec.getAttribute('id')
 
@@ -27,19 +32,22 @@ sections.forEach(sec => {
       links.classList.remove('active');
       document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
     })
-}})
+}
+menuIcon.classList.remove("bx-x")
+navbar.classList.remove("active")
+})
 
 
 let header = document.querySelector(".header");
 header.classList.toggle('sticky', window.scrollY > 100)
-};
+}
+
+
+
+
+
 let avatar = document.querySelector("#avatar");
 let berlin = document.querySelector(".berlin");
-
-
-
-
-
 
 avatar.addEventListener("mouseover", (e) =>
   {berlin.classList.add("slide-down")}
