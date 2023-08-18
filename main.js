a38f3527b84632de9119566003fffc187d3fb2c5
Etem Utku Oylum <utkuoylum@gmail.com>
2023-08-18 13:52:23 +0200

    header update

--- main.js
+++ main.js
@@ -7,12 +7,16 @@ let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = (e) => {
-  // sticky header
-  // remove toggle icon and navbar when click navbar links (scroll)
-  // animation footer on scroll
+let header = document.querySelector(".header");
+header.classList.toggle('sticky', window.scrollY > 100)
 };
 let avatar = document.querySelector("#avatar");
 let berlin = document.querySelector(".berlin");
+
+
+
+
+
 
 avatar.addEventListener("mouseover", (e) =>
   {berlin.classList.add("slide-down")}
