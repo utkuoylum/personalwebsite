9ef6a5438fb9ceb283c46112bb18606b497278e4
Etem Utku Oylum <utkuoylum@gmail.com>
2023-08-18 17:56:34 +0200

    func updates

--- main.js
+++ main.js
@@ -2,61 +2,48 @@
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
-
-menuIcon.addEventListener("click", e => {
-  menuIcon.classList.toggle("bx-x")
+menuIcon.addEventListener("click", (e) => {
+  menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
-})
+});
 // scroll sections
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
-
-
-
-
-
-
-
-
-
 window.onscroll = (e) => {
-sections.forEach(sec => {
-  let top = window.scrollY;
-  let offset = sec.offsetTop - 110;
-  let height = sec.offsetHeight;
-  let id = sec.getAttribute('id')
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 110;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-  if(top >= offset && top < offset + height) {
-    navLinks.forEach(links => {
-      links.classList.remove('active');
-      document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-    })
-}
-menuIcon.classList.remove("bx-x")
-navbar.classList.remove("active")
-})
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
+  });
 
-
-let header = document.querySelector(".header");
-header.classList.toggle('sticky', window.scrollY > 100)
-}
-
-
-
-
+  let header = document.querySelector(".header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+};
 
 let avatar = document.querySelector("#avatar");
 let berlin = document.querySelector(".berlin");
 
-avatar.addEventListener("mouseover", (e) =>
-  {berlin.classList.add("slide-down")}
-);
-avatar.addEventListener("mouseout", (e) =>
-  {berlin.classList.remove("slide-down")}
-);
+avatar.addEventListener("mouseover", (e) => {
+  berlin.classList.add("slide-down");
+});
+avatar.addEventListener("mouseout", (e) => {
+  berlin.classList.remove("slide-down");
+});
 
-let hireMe = document.querySelector('#hire-me');
+let hireMe = document.querySelector("#hire-me");
 hireMe.addEventListener("click", (e) => {
-  window.location.href = 'mailto:email@example.com';
-})
+  window.location.href = "mailto:email@example.com";
+});
